a4499b1b6ecdb6ab597d5ea130bd5e3d12b91cb5
Ealautzenhiser <Ealautzenhiser@gmail.com>
2016-03-24 21:46:39 -0500

    Doc to genomeGenerator
    Added documentation to genomeGenerator.py

--- genomeGenerator.py
+++ genomeGenerator.py
@@ -1,3 +1,38 @@
+"""
+
+takes lines of text and converts them into a genome and translation table,
+which are then converted into code.
+{} indicates that the next line should be inserted into the current code block.
+
+For instance:
+
+for {} in {}:
+x
+range({})
+10
+print {}
+{},{}
+"{}"
+Num:
+x
+
+would translate to (with codon length 3):
+translation = {'001':'for {} in {}', '002':'x', '003':'range({})', '004':'10',
+    '005':'print {}', '006':'{},{}', '007':'"{}"', '008':'Num:', '009':'x'}
+
+genome = '001002003004005006007008009'
+
+The reproduction framework would then translate the genome to:
+
+for x in range(10):
+    print "Num:",x
+
+When converting a genome into code, the indentation level is automatically
+increased whenever a line ends with a colon, but dedentation must be
+manually specified by a line 'dedent'
+
+"""
+
 inputDict = {}
 
 outputDict = {}
